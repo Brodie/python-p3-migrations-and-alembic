28035f198d359474bb4b9df347ecac19cd4d79ea
Brodie Ashcraft <brodiea19@gmail.com>
2023-08-05 10:41:58 -0700

    complete, moving onto next lesson

---
+++ lib/migrations/versions/f21dfd7b66af_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: f21dfd7b66af
+Revises: 
+Create Date: 2023-08-05 10:31:26.564507
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f21dfd7b66af'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
